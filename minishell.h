74dd968325f4f5ac04adacdaba506816fcb8a263
vez <lucientrillard77120@gmail.com>
2025-09-28 15:46:24 +0200

    f

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: ltrillar <ltrillar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 13:37:12 by ltrillar          #+#    #+#             */
-/*   Updated: 2025/09/28 15:02:08 by ltrillar         ###   ########.fr       */
+/*   Updated: 2025/09/28 15:46:18 by ltrillar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -171,7 +171,6 @@
 */
 
 char *getpath(char *buffer);
-pid_t get_from_path(char *buffer, char *path);
 int check_command(char *input);
 int try_input(char *input, char *envp[]);
 
