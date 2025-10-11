09db73cb4a610486b1b2560d57cc9d2d4eb9d155
Vez <vez@student.42.fr>
2025-10-11 02:33:13 +0200

    ifei

--- rl.c
+++ rl.c
@@ -9,48 +9,39 @@ char *rl(int fd)
     int line_len = 0;
     int i;
     
-    // Simple large buffer approach - no dynamic allocation during building
-    static char temp_line[100000]; // Large buffer to avoid malloc during line building
-    
-    // Check for invalid fd
+    static char temp_line[100000];
     if (fd < 0)
         return NULL;
     
     while (1)
     {
-        // If buffer is empty, read new data
         if (buffer_pos >= buffer_size)
         {
             buffer_size = read(fd, buffer, BUFFER_SIZE);
             buffer_pos = 0;
             
-            // If read error or EOF with no data collected
             if (buffer_size <= 0)
             {
                 if (line_len == 0)
                     return NULL;
-                break; // EOF but we have data to return
+                break;
             }
         }
         
-        // Process current buffer
         while (buffer_pos < buffer_size)
         {
             temp_line[line_len] = buffer[buffer_pos];
             buffer_pos++;
             line_len++;
             
-            // Found newline - finish the line
             if (temp_line[line_len - 1] == '\n')
             {
                 temp_line[line_len] = '\0';
                 
-                // Allocate final line
                 line = malloc(line_len + 1);
                 if (!line)
                     return NULL;
                 
-                // Copy line
                 i = 0;
                 while (i < line_len)
                 {
@@ -64,7 +55,6 @@ char *rl(int fd)
         }
     }
     
-    // EOF reached but we have data
     if (line_len > 0)
     {
         temp_line[line_len] = '\0';
