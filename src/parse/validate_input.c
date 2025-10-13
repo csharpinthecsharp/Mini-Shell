fa95b0f2f0ebaff48d67f3e7b5d178dfbd9ebcca
Lucien Trillard <ltrillar@c2r7s5.42luxembourg.lu>
2025-10-13 14:15:36 +0200

    dwdwd

--- src/parse/validate_input.c
+++ src/parse/validate_input.c
@@ -6,7 +6,7 @@
 /*   By: ltrillar <ltrillar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 12:46:28 by ltrillar          #+#    #+#             */
-/*   Updated: 2025/10/13 13:16:22 by ltrillar         ###   ########.fr       */
+/*   Updated: 2025/10/13 14:15:18 by ltrillar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,12 @@ static int pipe_check(char *input, int *pos)
     {
         print_error("syntax error near unexpected token `|'", "!");
         return (1);
-    } 
+    }
+    else if (input[ft_strlen(input) - 1] == PIPE)
+    {
+        print_error("syntax error, cannot end with a pipe", "!");
+        return (1);
+    }
     return (0);
 }
 
@@ -33,3 +38,4 @@ int global_check(t_data *d)
     }
     return (0);
 }
+ 
