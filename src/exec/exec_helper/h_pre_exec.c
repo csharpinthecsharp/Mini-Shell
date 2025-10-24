bf475a0b83090c2f265b841e74ab348dc0cd43b0
vez <lucientrillard77120@gmail.com>
2025-10-24 17:06:22 +0200

    more cleanup

--- src/exec/exec_helper/h_pre_exec.c
+++ src/exec/exec_helper/h_pre_exec.c
@@ -6,7 +6,7 @@
 /*   By: ltrillar <ltrillar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 16:47:20 by ltrillar          #+#    #+#             */
-/*   Updated: 2025/10/23 17:38:20 by ltrillar         ###   ########.fr       */
+/*   Updated: 2025/10/24 17:05:52 by ltrillar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,34 +33,13 @@ char *get_directory(const char *path)
     return dir;
 }
 
-char **fix_redir_arg(t_cmd *cmd)
+int count_redir_arg(t_cmd *cmd)
 {
-    int i = 0;
-    int arg_count = 0;
+    int i;
+    int arg_count;
 
-    while (i < cmd->nb_arg)
-    {
-        if ((!ft_strncmp(cmd->arg[i], ">>", 3) ||
-             !ft_strncmp(cmd->arg[i], "<<", 3) ||
-             !ft_strncmp(cmd->arg[i], ">", 2)  ||
-             !ft_strncmp(cmd->arg[i], "<", 2)) &&
-            cmd->arg[i + 1]) // make sure filename exists
-        {
-            i += 2; // skip operator + filename
-        }
-        else {
-            arg_count++;
-            i++;
-        }
-    }
-
-    char **dup = malloc(sizeof(char *) * (arg_count + 1));
-    if (!dup)
-        return NULL;
-
-    // Second pass: copy only non-redirection args
+    arg_count = 0;
     i = 0;
-    int arg_j = 0;
     while (i < cmd->nb_arg)
     {
         if ((!ft_strncmp(cmd->arg[i], ">>", 3) ||
@@ -70,76 +49,98 @@ char **fix_redir_arg(t_cmd *cmd)
             cmd->arg[i + 1])
         {
             i += 2;
+        }
+        else {
+            arg_count++;
+            i++;
+        }
+    }
+    return (arg_count);
+}
+
+int is_redir_and_next(char **argv, int i)
+{
+    if ((!ft_strncmp(argv[i], ">>", 3) ||
+        !ft_strncmp(argv[i], "<<", 3) ||
+        !ft_strncmp(argv[i], ">", 2)  ||
+        !ft_strncmp(argv[i], "<", 2)) &&
+    argv[i + 1])
+        return (SUCCESS);
+    return (FAILED);
+
+}
+char **fix_redir_arg(t_cmd *cmd)
+{
+    int i;
+    int arg_count;
+    int arg_j;
+
+    i = 0;
+    arg_j = 0;
+    arg_count = count_redir_arg(cmd);
+    char **dup = malloc(sizeof(char *) * (arg_count + 1));
+    if (!dup)
+        return NULL;
+    while (i < cmd->nb_arg)
+    {
+        if (is_redir_and_next(cmd->arg, i) == SUCCESS)
+        {
+            i += 2;
             continue;
         }
         dup[arg_j++] = ft_strdup(cmd->arg[i]);
         i++;
     }
     dup[arg_j] = NULL;
-
-    // update nb_arg to new count
     cmd->nb_arg = arg_count;
+    return (dup);
+}
 
-    return dup;
+static void set_error(t_data *d, int exit_code, char *error_s, char *tmp)
+{
+    d->exit_status = exit_code;
+    print_error(error_s, tmp);
+}
+
+static int stat_error_h(t_data *d, char *tmp)
+{
+    struct stat st;
+    if (stat(tmp, &st) == -1)
+    {
+        if (errno == ENOENT && ft_strchr(tmp, '/') != NULL)
+            set_error(d, 127, "No such file or directory", tmp);
+        else if (errno == EACCES)
+            set_error(d, 126, "Permission denied", tmp);
+        else
+            set_error(d, 127, "command not found", tmp);
+        return (FAILED);
+    }
+    if (S_ISDIR(st.st_mode))
+    {
+        if (ft_strchr(tmp, '/') != NULL)
+            set_error(d, 126, "Is a directory", tmp);
+        else
+            set_error(d, 127, "command not found", tmp);
+        return (FAILED);
+    }
+    return (SUCCESS);
 }
 
 static int error_h(t_data *d, char *tmp)
 {
     if (ft_strchr(tmp, '/') == NULL)
     {
-        d->exit_status = 127;
-        print_error("command not found", tmp);
+        set_error(d, 127, "command not found", tmp);
         return (FAILED);
     }
-    
-    struct stat st;
-    if (stat(tmp, &st) == -1)
-    {
-        if (errno == ENOENT && ft_strchr(tmp, '/') != NULL)
-        {
-            d->exit_status = 127;
-            print_error("No such file or directory", tmp);
-        }
-        else if (errno == EACCES)
-        {
-            d->exit_status = 126;
-            print_error("Permission denied", tmp);
-        }
-        else
-        {
-            d->exit_status = 127;
-            print_error("command not found", tmp);
-        }
+    if (stat_error_h(d, tmp) == FAILED)
         return (FAILED);
-    }
-
-    if (S_ISDIR(st.st_mode))
-    {
-        if (ft_strchr(tmp, '/') != NULL)
-        {
-            d->exit_status = 126;
-            print_error("Is a directory", tmp);
-        }
-        else
-        {
-            d->exit_status = 127;
-            print_error("command not found", tmp);
-        }
-        return (FAILED);
-    }
-
     if (access(tmp, X_OK) != 0)
     {
         if (errno == EACCES)
-        {
-            d->exit_status = 126;
-            print_error("Permission denied", tmp);
-        }
+            set_error(d, 126, "Permission denied", tmp);
         else
-        {
-            d->exit_status = 127;
-            print_error("command not found", tmp);
-        }
+            set_error(d, 127, "command not found", tmp);
         return (FAILED);
     }
     return (SUCCESS);
