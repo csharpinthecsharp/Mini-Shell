6295d25e47c51911a40148c342fa9af90fa5f88f
csharpinthecsharp <trilllucien@gmail.com>
2025-10-27 18:53:19 +0100

    ojdz

--- minishell/src/parse/parse_input/p_envvar.c
+++ minishell/src/parse/parse_input/p_envvar.c
@@ -6,7 +6,7 @@
 /*   By: ltrillar <ltrillar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 15:32:50 by ltrillar          #+#    #+#             */
-/*   Updated: 2025/10/27 17:06:06 by ltrillar         ###   ########.fr       */
+/*   Updated: 2025/10/27 18:52:48 by ltrillar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,32 +43,39 @@ int	get_expanded_size(char *s, t_data *d)
 
 char	*get_env_string(t_data *d, char *s)
 {
-	int		i;
-	int		j;
+	int		i = 0;
+	int		j = 0;
 	char	*arg;
 	int		env_index;
+	char	*env_val;
 
 	arg = malloc(sizeof(char) * (ft_strlen(s) + 1));
-	if (handle_error_malloc(arg) == FAILED)
+	if (!arg)
 		return (NULL);
-	i = 0;
-	j = 0;
 	if (s[i] == '$')
 		i++;
 	while (s[i] && !ft_isspace(s[i]) && s[i] != '$')
 		arg[j++] = s[i++];
 	arg[j] = '\0';
+
 	env_index = 0;
 	while (d->envp[env_index])
 	{
 		if (ft_strncmp(d->envp[env_index], arg, ft_strlen(arg)) == 0
 			&& d->envp[env_index][ft_strlen(arg)] == '=')
-			return (ft_strdup(ft_get_env(d, arg)));
+		{
+			env_val = ft_get_env(d, arg);
+			free(arg);
+			if (!env_val)
+				return (ft_strdup(""));
+			return (ft_strdup(env_val));
+		}
 		env_index++;
 	}
 	free(arg);
 	return (ft_strdup(""));
 }
+
 
 static int	process_dollar(char **ps, t_data *d, char **arg, int *j)
 {
