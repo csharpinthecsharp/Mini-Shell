f58c74bde922282490c076b79024437307adb9df
csharpinthecsharp <trilllucien@gmail.com>
2025-11-03 16:29:42 +0100

    Fixed <<echo oi and CTRL C, was writting 'n'

--- src/exec/exec_helper/h_pre_exec_extra.c
+++ src/exec/exec_helper/h_pre_exec_extra.c
@@ -12,10 +12,97 @@
 
 #include "../../../include/minishell.h"
 
+static t_error	*create_error_node(int exit_code, const char *message,
+					const char *argument)
+{
+	t_error	*node;
+
+	node = malloc(sizeof * node);
+	if (!node)
+		return (NULL);
+	node->message = ft_strdup(message);
+	if (!node->message)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->argument = ft_strdup(argument);
+	if (!node->argument)
+	{
+		free(node->message);
+		free(node);
+		return (NULL);
+	}
+	node->exit_code = exit_code;
+	node->next = NULL;
+	return (node);
+}
+
+void	add_deferred_error(t_data *d, int exit_code, const char *message,
+			const char *argument)
+{
+	t_error	*node;
+	t_error	*tail;
+
+	if (!d)
+		return ;
+	node = create_error_node(exit_code, message, argument);
+	if (!node)
+	{
+		print_error(message, argument);
+		return ;
+	}
+	if (!d->errors)
+		d->errors = node;
+	else
+	{
+		tail = d->errors;
+		while (tail->next)
+			tail = tail->next;
+		tail->next = node;
+	}
+}
+
+void	flush_deferred_errors(t_data *d)
+{
+	t_error	*node;
+
+	if (!d)
+		return ;
+	while (d->errors)
+	{
+		node = d->errors;
+		d->errors = node->next;
+		print_error(node->message, node->argument);
+		free(node->message);
+		free(node->argument);
+		free(node);
+	}
+}
+
+void	clear_deferred_errors(t_data *d)
+{
+	t_error	*node;
+
+	if (!d)
+		return ;
+	while (d->errors)
+	{
+		node = d->errors;
+		d->errors = node->next;
+		free(node->message);
+		free(node->argument);
+		free(node);
+	}
+}
+
 void	set_error(t_data *d, int exit_code, char *error_s, char *tmp)
 {
 	d->exit_status = exit_code;
-	print_error(error_s, tmp);
+	if (d->defer_errors)
+		add_deferred_error(d, exit_code, error_s, tmp);
+	else
+		print_error(error_s, tmp);
 }
 
 int	stat_error_h(t_data *d, char *tmp)
--- src/exec/exec_main/execute.c
+++ src/exec/exec_main/execute.c
@@ -86,6 +86,26 @@ void	exec_built_inpipe(int **pipe, t_data *d, int n_pipe, int *pos)
 		perror("fork failed");
 }
 
+void	exec_missing_bin(int **pipe, t_data *d, int n_pipe, int *pos)
+{
+	pid_t	pid;
+
+	handle_heredocs(d, pos);
+	d->fd_out = 0;
+	d->fd_in = 0;
+	pid = fork();
+	if (pid == 0)
+	{
+		handle_pipe(pipe, pos, n_pipe);
+		handle_redirections(d, pos, &d->fd_out, &d->fd_in);
+		exit(d->exit_status);
+	}
+	else if (pid > 0)
+		d->last_fork_pid = pid;
+	else
+		perror("fork failed");
+}
+
 void	execute_command_by_type(int **pipe, t_data *d, int n_pipe, int *pos)
 {
 	if (check_output_ofeach(&d->cmd[*pos], d) == FAILED)
@@ -96,6 +116,8 @@ void	execute_command_by_type(int **pipe, t_data *d, int n_pipe, int *pos)
 		exec_built_inpipe(pipe, d, n_pipe, pos);
 	else if (d->cmd[*pos].state_cmd == ALONE_REDIR)
 		exec_alone_redir_inpipe(pipe, d, n_pipe, pos);
+	else if (d->cmd[*pos].state_cmd == MISSING_BIN)
+		exec_missing_bin(pipe, d, n_pipe, pos);
 }
 
 void	start_execution(t_data *d)
